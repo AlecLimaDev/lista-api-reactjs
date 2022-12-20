b69429e9599b4ef2c6a5b11ff095c4049209e09e
Alec Lima <aleclimadev@gmail.com>
2022-12-20 10:00:33 -0300

    componente da page criados

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import { Link, NavLink } from "react-router-dom";
+
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <NavLink
+        className={({ isActive }) => (isActive ? "active" : undefined)}
+        to="/"
+      >
+        Home
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "active" : undefined)}
+        to="/about"
+      >
+        Sobre
+      </NavLink>
+    </nav>
+  );
+};
+
+export default Navbar;
